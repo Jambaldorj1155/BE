68f26e8f3b8f95dafd9c62a69990d60b8dc0f379
Jambalorj1155 <jambaldorj1155@gmail.com>
2024-08-01 18:02:36 +0800

    making difference

--- BE-expense-tracker/index.js
+++ BE-expense-tracker/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import fs from "node:fs";
 import cors from "cors";
 import { db } from "./db.js";
+import { error } from "node:console";
 
 const app = express();
 const port = 8000;
@@ -50,6 +51,21 @@ app.get("/users/createTable", async (req, res) => {
   
 });
 
+app.get("/users", async (req, res) => {
+  const queryText = `
+  SELECT * FROM users
+  `;
+
+  try {
+    const result = await db.query(queryText);
+    res.send(result.rows);
+  } catch (error) {
+    console.log(error);
+    
+  }
+
+ 
+});
 
 
 // app.get("/", async (req, res) => {
@@ -94,20 +110,7 @@ app.post("/users/create", async (req, res) => {
   
 });
 
-// app.get("/getUsers", async (req, res) => {
-//   const queryText = `
-//   SELECT * FROM users
-//   `;
 
-//   try {
-//     const result = await db.query(queryText);
-//     res.send(result.rows);
-//   } catch (error) {
-//     console.log(error);
-//   }
-
- 
-// });
 
 
 
@@ -131,6 +134,28 @@ app.post("/users/create", async (req, res) => {
 //     });
 
 //     })
+
+app.put("/users/:id", async (req, res) => {
+  const {id} = req.params;
+  const {name, email} = req.body;
+  try {
+    const result = await db.query(
+      "UPDATE users SET name = $1, email = $2, WHERE id = $3 RETURNING *", [name, email, id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({error: "Item not found"});
+
+    } else { res.status(200).json(result.rows[0]);
+
+    }
+  } catch (erro) {
+    console.error(erro);
+    res.status(500).json({error:"Database error"});
+  }
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
